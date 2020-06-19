19a229369f8432749e685cf3588b7b832c3b6a7f
Souvik44 <54095866+Souvik44@users.noreply.github.com>
2020-06-19 22:53:47 +0530

    Salimo is added

--- abc.php
+++ abc.php
@@ -1,6 +1,6 @@
 <?php
   echo "Smart is new thing";
   echo "Hard Luck to Lose you brother!!!!";
-
- echo "nice to have you here!!!!";
+  echo "nice to have you here!!!!";
+  echo "Salimo is here";
 ?>
